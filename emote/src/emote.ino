65e06a8b291584aae1119dee655f85588710a785
Andy Artz <andyartz@andyartz.com>
2018-03-30 16:43:57 -0500

    Update emote.ino

--- emote/src/emote.ino
+++ emote/src/emote.ino
@@ -1,1 +1,23 @@
+const String POOP = "💩";
+const String OUTBOUND = "twilio";
 
+void setup() {
+  emitPoop();
+}
+
+void emitPoop() {
+  emit(OUTBOUND, POOP);
+}
+
+void loop() {
+  log("Sent message.");
+}
+
+void emit(String channel, String message) {
+  Particle.publish(channel, message, PRIVATE);
+  log("Emitted on channel '" + channel + "': " + message);
+}
+
+void log(String message) {
+  Serial.println(message);
+}
