d5e1a3c218bc9d8923452788e81aa503b453d45a
Andy Artz <aartz@identifix.com>
2018-04-08 18:43:44 -0500

    Add all three switches. Change KISS to THUMBS_UP. Split MQTT channels based on device.

--- emote/src/emote.ino
+++ emote/src/emote.ino
@@ -1,33 +1,36 @@
 #include <MQTT.h>
 
+const String VERSION = "v0.0.7";
+
 const int BAUD_RATE = 115200;
 
 const String THINGSPEAK_USERNAME = "ezequiell";
 const String THINGSPEAK_MQTT_API_KEY = "KJD6QV2N7PJ2YM7K";
 
-const String EMOTE_EVENT_PARTICLE_WEBHOOK_NAME = "emote";
-const String EMOTE_EVENT_THINGSPEAK_CHANNEL_NUMBER = "467440";
-const String EMOTE_EVENT_THINGSPEAK_CHANNEL_API_READ_KEY = "80HXPU3QJX8H7CJF";
+const String OUTBOUND_EVENTS_WEBHOOK_NAME = "twilio";
+
+const String EMOTE_EVENT_THINGSPEAK_CHANNEL_NUMBER_FLOWER = "467440";
+const String EMOTE_EVENT_THINGSPEAK_CHANNEL_API_READ_KEY_FLOWER = "80HXPU3QJX8H7CJF";
+const String EMOTE_EVENT_THINGSPEAK_CHANNEL_NUMBER_TARDIS = "469924";
+const String EMOTE_EVENT_THINGSPEAK_CHANNEL_API_READ_KEY_TARDIS = "EKTNY1SLEPZ4A9CM";
+
 const String EMOTE_EVENT_THINGSPEAK_CHANNEL_FIELD = "field1";
-const String EMOTE_EVENT_THINGSPEAK_CHANNEL = "channels/"
-                                              + EMOTE_EVENT_THINGSPEAK_CHANNEL_NUMBER
-                                              + "/subscribe/fields/"
-                                              + EMOTE_EVENT_THINGSPEAK_CHANNEL_FIELD
-                                              + "/"
-                                              + EMOTE_EVENT_THINGSPEAK_CHANNEL_API_READ_KEY;
 
 const int THINGSPEAK_MQTT_PORT = 1883;
-const int REMOTE_CONFIG_PIN = D0;
 
+// Pins ////////////////////////////////////////////////////////////////////////
+const int REMOTE_CONFIG_PIN = D0;
+const int SWITCH_PIN_0 = A0;
+const int SWITCH_PIN_1 = A1;
+const int SWITCH_PIN_2 = A2;
+
+// Emoji ///////////////////////////////////////////////////////////////////////
 const String POOP = "💩";
 const String HEART = "❤️";
 const String SAD = "☹️";
-const String KISS = "😘";
+const String THUMBS_UP = "👍";
 const String CLAP = "👏";
 const String SMILE = "😀";
-
-const String OUTBOUND = "twilio";
-const String INBOUND = "twilio"; // TODO
 
 const int ONE_SECOND_IN_MILLIS = 1000;
 
@@ -37,23 +40,43 @@ const int TARDIS = 1;
 int config = 0;
 String name = "none";
 
-StaticJsonBuffer<200> jsonBuffer;
+// switch fields ///////////////////////////////////////////////////////////////
+bool lastState0 = FALSE;
+bool currentState0 = FALSE;
+bool lastState1 = FALSE;
+bool currentState1 = FALSE;
+bool lastState2 = FALSE;
+bool currentState2 = FALSE;
+
 MQTT client("mqtt.thingspeak.com", THINGSPEAK_MQTT_PORT, mqttEventHandler);
 
 void setup() {
 
-  emit("version", "v0.0.2");
+  emit("version", VERSION);
 
+  setupSwitches();
   setupSerialConnection();
   doConfig();
-  subscribeMQTT();
+  setupMQTT();
+}
+
+void setupSwitches() {
+  setupSwitch(SWITCH_PIN_0);
+  setupSwitch(SWITCH_PIN_1);
+  setupSwitch(SWITCH_PIN_2);
+}
+
+void setupSwitch(int switchPin) {
+  pinMode(switchPin, INPUT_PULLDOWN);
 }
 
 // config //////////////////////////////////////////////////////////////////////
 
 void doConfig() {
+
   setupInputPin(REMOTE_CONFIG_PIN);
   config = digitalRead(REMOTE_CONFIG_PIN);
+
   if (isFlower()) {
     name == "Flower";
     emit("config", "Device is a flower.");
@@ -73,23 +96,110 @@ bool isTardis() {
   return config == TARDIS;
 }
 
-////////////////////////////////////////////////////////////////////////////////
+// Thingspeak Publish/Subscribe ////////////////////////////////////////////////
 
-void subscribeMQTT() {
+void setupMQTT() {
     client.connect( name, THINGSPEAK_USERNAME, THINGSPEAK_MQTT_API_KEY, NULL, MQTT::QOS0, 0, NULL, true);
 
     delay( ONE_SECOND_IN_MILLIS );
 
     if ( client.isConnected()) {
-      String subscribed = String(client.subscribe( EMOTE_EVENT_THINGSPEAK_CHANNEL ));
+      String subscribed = subscribeToMQTT();
       emit("subscribed", subscribed);
     } else {
       emit("subscribed", SAD + "ERROR! Thingspeak MQTT connection failed.");
     }
 }
 
+String subscribeToMQTT() {
+  return String(client.subscribe(getThingspeakReadChannel()));
+}
+
+void send(String message) {
+  emit(OUTBOUND_EVENTS_WEBHOOK_NAME, message);
+}
+
+String getThingspeakReadChannel() {
+  return "channels/" + getChannelNumber() + "/subscribe/fields/" + getChannel() + "/" + getApiKey();
+}
+
+String getChannelNumber() {
+  if (isFlower()) {
+    return EMOTE_EVENT_THINGSPEAK_CHANNEL_NUMBER_FLOWER;
+  } else if (isTardis()) {
+    return EMOTE_EVENT_THINGSPEAK_CHANNEL_NUMBER_TARDIS;
+  } else {
+    emit("error", "No Channel Number is available for config: " + config);
+    return "";
+  }
+}
+
+String getChannel() {
+  return EMOTE_EVENT_THINGSPEAK_CHANNEL_FIELD;
+}
+
+String getApiKey() {
+  if (isFlower()) {
+    return EMOTE_EVENT_THINGSPEAK_CHANNEL_API_READ_KEY_FLOWER;
+  } else if (isTardis()) {
+    return EMOTE_EVENT_THINGSPEAK_CHANNEL_API_READ_KEY_TARDIS;
+  } else {
+    emit("error", "No API Key is available for config: " + config);
+    return "";
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 void loop() {
    client.loop();
+
+   lastState0 = currentState0;
+   lastState1 = currentState1;
+   lastState2 = currentState2;
+   currentState0 = switchPressed(SWITCH_PIN_0);
+   currentState1 = switchPressed(SWITCH_PIN_1);
+   currentState2 = switchPressed(SWITCH_PIN_2);
+
+   if (switchPressedJustNow(SWITCH_PIN_0)) {
+     if (isTardis()) {
+       send(HEART);
+     } else if (isFlower()) {
+       send(SMILE);
+     }
+   }
+
+  if (switchPressedJustNow(SWITCH_PIN_1)) {
+    if (isTardis()) {
+      send(THUMBS_UP);
+    } else if (isFlower()) {
+      send(CLAP);
+    }
+  }
+
+  if (switchPressedJustNow(SWITCH_PIN_2)) {
+    if (isTardis()) {
+      send(POOP);
+    } else if (isFlower()) {
+      send(SAD);
+    }
+  }
+  waitSeconds(1);
+}
+
+bool switchPressedJustNow(int pin) {
+  if (pin == SWITCH_PIN_0) {
+    return !lastState0 && currentState0;
+  } else if (pin == SWITCH_PIN_1) {
+    return !lastState1 && currentState1;
+  } else if (pin == SWITCH_PIN_2) {
+    return !lastState2 && currentState2;
+  } else {
+    return FALSE;
+  }
+}
+
+bool switchPressed(int pin) {
+  return digitalRead(pin) == 1;
 }
 
 void mqttEventHandler( char* topic, byte* payload, unsigned int length ) {
@@ -112,8 +222,8 @@ void handleAsFlower(String emoji) {
     handlePoop();
   } else if (emoji == HEART) {
     handleHeart();
-  } else if (emoji == KISS) {
-    handleKiss();
+  } else if (emoji == THUMBS_UP) {
+    handleTHUMBS_UP();
   } else {
     handleUnknown(emoji);
   }
@@ -133,7 +243,7 @@ void handleAsTardis(String emoji) {
 }
 
 void handleUnknown(String payload) {
-  emit("mqtt", payload);
+  emit("mqtt", "I don't do " + payload);
 }
 
 // Flower handlers /////////////////////////////////////////////////////////////
@@ -146,8 +256,8 @@ void handleHeart() {
   emit("mqtt", HEART);
 }
 
-void handleKiss() {
-  emit("mqtt", KISS);
+void handleTHUMBS_UP() {
+  emit("mqtt", THUMBS_UP);
 }
 
 // Tardis handlers /////////////////////////////////////////////////////////////
@@ -162,23 +272,6 @@ void handleSmile() {
 
 void handleSad() {
   emit("mqtt", SAD);
-}
-
-
-// void handleInboundMessage(const char *event, const char *data) {
-//   log("Incoming message detected!!!");
-//   log(data);
-//   JsonObject& root = jsonBuffer.parseObject((char*)data);
-//   if (!root.success()) {
-//     log("Couldn't parse incoming message :(");
-//     return;
-//   }
-//   const char* jsonData = root["data"];
-//   log(jsonData);
-// }
-
-void send(String message) {
-  emit(OUTBOUND, message);
 }
 
 // Utility Code Starts here ///////////////////////////////////////////////
